db51fac5cbd408dae3a59beddd66e3dd5a3fcb80
sagar <sagar.raval@raweng.com>
2017-10-27 23:57:59 +0530

    added user module

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const http = require('http');
+const cookieParser = require('cookie-parser');
+const validator = require('express-validator');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const container = require('./container');
+const passport = require('passport');
+
+container.resolve(function(users) {
+
+  mongoose.Promise = global.Promise;
+
+  mongoose.connect('mongodb://localhost/footballkik', { useMongoClient:true });
+
+  const app = setupExpress();
+
+  function setupExpress(){
+    const app = express();
+    const server = http.createServer(app);
+    server.listen(3080, () => {
+      console.log('Listening on port 3080');
+    });
+    ConfigureExpress(app);
+
+    const router = require('express-promise-router')();
+    users.SetRouting(router);
+    app.use(router);
+  }
+
+  function ConfigureExpress(app){
+    app.use(express.static('public'));
+    app.use(cookieParser());
+    app.set('view engine', 'ejs');
+    app.use(bodyParser.json());
+    app.use(validator());
+    app.use(session({
+      secret:'fullmetal%$@&',
+      resave: true,
+      saveUninitialized: false,
+      store: new MongoStore({ mongooseConnection: mongoose.connection})
+    }))
+    app.use(flash());
+    app.use(passport.initialize());
+    app.use(passport.session());
+  }
+
+});
